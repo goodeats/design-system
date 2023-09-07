5c5098b45e88d39174a17d0971576b10f21751e6
Pat Needham <patrick.h.needham@gmail.com>
2023-09-07 13:37:20 -0400

    can import from package.json in other projects

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,24 @@ import React from 'react';
 
 export interface ButtonProps {
   label: string;
+  type: 'button' | 'submit' | 'reset';
 }
 
+const defaultStyle = {
+  backgroundColor: '#000',
+  color: '#fff',
+  border: 'none',
+  padding: '10px 20px',
+  borderRadius: '5px',
+  cursor: 'pointer',
+};
+
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (
+    <button style={defaultStyle} type={props.type}>
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
